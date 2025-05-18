9760bede2d123dff82dfd827828699a1bd44e201
jordanenyirenda <jnyirenda1@my.bcit.ca>
2025-05-18 16:20:40 -0700

    onbording.css page added

--- app/onboarding/page.jsx
+++ app/onboarding/page.jsx
@@ -1,35 +1,22 @@
+"use client";
 import React from "react";
-import "@/app/globals.css";
+import { useRouter } from "next/navigation";
 import Button from "@/components/Button/button";
+import styles from "@/app/globals.css"
 
-function ContinueButton( {
-    label = "Continue",
-    variant = "oceanButton",
-    onClick = () => {},
-    href,
-    textColor = "#ffffff",
-    border = "none",
-    borderRadius = "12px",
-    showOverlay = false,
-    overlayContent = "Overlay Content",
-    size = "lg",
-    }) {
-    const router = useRouter();
-    
-    return (
-        <Button
-        label={label}
-        variant={variant}
-        onClick={() => {
-            router.push(href);
-            onClick();
-        }}
-        textColor={textColor}
-        border={border}
-        borderRadius={borderRadius}
-        showOverlay={showOverlay}
-        overlayContent={overlayContent}
-        size={size}
-        />
-    );
-};
+
+
+export default function OnboardingPage (){
+    return(
+
+        <div className="OnboardingBackground" background="img/">
+            
+            <p className="">The marketplace where those who need</p>
+            <p className="">it done, meet those who get it done.</p>
+           <Button 
+             label="Continue">
+           </Button>
+
+        </div>
+    ); 
+}
