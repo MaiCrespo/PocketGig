d10450ec938f1a352025a2aea2ee36fbe8fcc5e2
jordanenyirenda <jnyirenda1@my.bcit.ca>
2025-05-18 17:55:05 -0700

    button aligned center

--- app/onboarding/page.jsx
+++ app/onboarding/page.jsx
@@ -5,23 +5,19 @@ import Button from "@/components/Button/button";
 import styles from "@/app/globals.css"
 import "@/app/onboarding/onboarding.css";
 
-
-
 export default function OnboardingPage (){
+    const router = useRouter();
+    
     return(
-
         <div className="OnboardingBackground" background="img/">
-            
             <p className="">The marketplace where those who need it done, meet those who get it done.</p>
 
             <div className="buttonContainer">
                 <Button 
-                    label="Continue">
-                    onClick={() => Router.push("/onboarding/step2")}
-                </Button>
+                    label="Continue"
+                    onClick={() => router.push("/onboarding/step2")}
+                />
             </div>
-           
-
         </div>
     ); 
 }
