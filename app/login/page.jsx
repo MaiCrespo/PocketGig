a999241c27b083c80156d8b5f04d276ed882ff9d
jordanenyirenda <jnyirenda1@my.bcit.ca>
2025-05-18 22:23:16 -0700

    material input feild styled

---
+++ app/login/page.jsx
@@ -0,0 +1,8 @@
+"use client";
+import React from "react";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+import Button from "@/components/Button/button";
+import styles from "@/app/globals.css"
+import "@/app/onboarding/login.css";
+export default function LoginPage(){
