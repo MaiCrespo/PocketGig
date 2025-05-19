2d4e7c3bdd641c6617fe72d77ff1f0c07891d37b
monavi77 <vmonakova@my.bcit.ca>
2025-05-18 18:19:47 -0700

    Refactor Home component layout and replace placeholder image with Empty.jpg for better asset management. Add tabs to switch between GigSeeker and GigAssigner.

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,6 @@
 "use client";
-
-import Header from "@/components/Header";
+import TopNav from "@/components/TopNav";
+import RoleTabs from "@/components/RoleTabs";
 import SearchBar from "@/components/SearchBar";
 import SeekerCard from "@/components/SeekerCard";
 import { Button } from "@mui/material";
@@ -8,16 +8,15 @@ import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 import TuneIcon from "@mui/icons-material/Tune";
 import { mockSeekerData } from "./mockData";
 import { useRouter } from "next/navigation";
-import Tabs from "@/components/Tabs";
 import BottomNav from "@/components/BottomNav";
 
 export default function Home() {
   const router = useRouter();
 
   return (
-    <>
-      <Header />
-      <Tabs />
+    <div className="bg-[#F8FAFC] flex flex-col pb-20 pt-16">
+      <TopNav />
+      <RoleTabs/>
 
       {/* My Activity Section */}
       <section className="p-4">
@@ -25,7 +24,7 @@ export default function Home() {
         <p className="text-sm text-gray-500 mb-4">Most Recent</p>
         <div className="flex flex-col items-center text-center text-gray-500">
           <img
-            src="https://via.placeholder.com/150"
+            src="/Empty.jpg"
             alt="No Activity"
             className="w-24 h-24 mb-2"
           />
@@ -65,13 +64,13 @@ export default function Home() {
         </div>
         <button
           onClick={() => router.push("/gig-posting")}
-          className=" mb-18 absolute bottom-4 left-1/2 transform -translate-x-1/2 bg-blue-500 text-white px-4 py-2 rounded-2xl shadow-lg w-70 h-10 z-10 cursor-pointer"
+          className=" fixed mb-18 absolute bottom-4 left-1/2 transform -translate-x-1/2 bg-blue-500 text-white px-4 py-2 rounded-2xl shadow-lg w-70 h-10 z-10 cursor-pointer"
         >
           Create Gig
         </button>
       </section>
 
       <BottomNav />
-    </>
+      </div>
   );
 }
