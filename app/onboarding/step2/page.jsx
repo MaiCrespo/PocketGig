d45441f120608509ca0c6af78c7a511af190f22f
jordanenyirenda <jnyirenda1@my.bcit.ca>
2025-05-18 19:40:23 -0700

    imports added to step 2

---
+++ app/onboarding/step2/page.jsx
@@ -0,0 +1,7 @@
+"use client";
+import React from "react";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+import Button from "@/components/Button/button";
+import styles from "@/app/globals.css"
+import "@/app/onboarding/onboarding.css";
