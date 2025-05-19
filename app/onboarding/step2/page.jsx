6f36990aef821c74229efe1e39ed6997456f4061
jordanenyirenda <jnyirenda1@my.bcit.ca>
2025-05-18 19:47:12 -0700

    step 2 background & button styling

--- app/onboarding/step2/page.jsx
+++ app/onboarding/step2/page.jsx
@@ -4,4 +4,21 @@ import { useRouter } from "next/navigation";
 import Image from "next/image";
 import Button from "@/components/Button/button";
 import styles from "@/app/globals.css"
-import "@/app/onboarding/onboarding.css";
+import "@/app/onboarding/step2/step2.css";
+
+export default function Step2Page() {
+    const router = useRouter();
+
+    return(
+        <div className="OnboardingBackground">
+            <div className="gradient-overlay"></div> 
+            <div className="buttonContainer">
+                <Button 
+                    label="Continue"
+                    onClick={() => router.push("/onboarding/login")}
+                />
+            </div>
+        </div>
+    );
+}
+
