a889b85bc388dc9e1ae0f640037379ee2a0181d8
jordanenyirenda <jnyirenda1@my.bcit.ca>
2025-05-18 18:44:47 -0700

    moving image inserted

--- app/onboarding/page.jsx
+++ app/onboarding/page.jsx
@@ -10,7 +10,7 @@ export default function OnboardingPage (){
     const router = useRouter();
     
     return(
-        <div className="OnboardingBackground" background="img/">
+        <div className="OnboardingBackground">
             <Image 
                 src="/images/lgWhiteLogo.svg"
                 alt="PocketGig Logo"
