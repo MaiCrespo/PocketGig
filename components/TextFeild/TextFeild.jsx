60ba020ff79e44cede5ee3abae34b9c07a171a81
jordanenyirenda <jnyirenda1@my.bcit.ca>
2025-05-22 19:20:49 -0700

    rebasing

--- components/TextFeild/TextFeild.jsx
+++ components/TextFeild/TextFeild.jsx
@@ -1,8 +1,7 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
-import Button from "@/components/Button/button";
 import styles from "@/app/globals.css"
 
 const TextField = () => {
