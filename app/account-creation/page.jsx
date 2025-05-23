52a6801786ad6013a63b6f006ca3a28d31665c4a
jordanenyirenda <jnyirenda1@my.bcit.ca>
2025-05-22 18:49:54 -0700

    account creation page

--- app/account-creation/page.jsx
+++ app/account-creation/page.jsx
@@ -8,22 +8,51 @@ import TopNav from '@/components/TopNav';
 import { imageConfigDefault } from "next/dist/shared/lib/image-config";
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import MenuIcon from '@mui/icons-material/Menu';
+import Stepper from "@/components/Stepper";
 //import Image from "/logo.png";//
+
 
 
 export default function AccountCreationPage() {
     const router = useRouter();
 
     return(
-            <header className="flex items-center justify-between px-4 py-3 bg-white shadow-sm">
+<div className="account-creation-container bg-white" style={{height: "100vh"}}>
+        <div>
+             <header className="flex items-center justify-between px-4 py-3 bg-white shadow-sm">
               <div className="flex items-center gap-2">
                 <img src="/logo.png" alt="PocketGig Logo" className="w-8 h-8" />
                 <span className="font-bold text-xl text-[#003366]">PocketGig</span>
-              </div>
-              <div className="flex items-center gap-4">
-               
-    
-              </div>
+                 <div className="flex items-center gap-4"></div>
+             </div>
             </header>
+          </div>
+        
+
+        <h2 className="text-left ml-10 mt-10 font-medium text-2xl text-[#003366]">Create an Account</h2>
+    
+        
+        <Stepper
+            steps={["Step 1", "Step 2", "Step 3"]}
+            currentStep={1}
+       />
+       <div className="acccount-form-container" style={{display: "flex", flexDirection: "column", alignItems: "center", marginTop:"60px"}}>
+            <input type="text" placeholder="First Name" className="firstNameinput" style={{width: "358px", height: "48px", border:"1px solid var(--inner-blue)", marginTop:"20px", borderRadius:"8px" }} />
+            <input type="text" placeholder="Last Name" className="lastNameInput" style={{width: "358px", height: "48px", border:"1px solid var(--inner-blue)", marginTop:"20px", borderRadius:"8px"}} />
+            <input type="email" placeholder="Email" className="emailInput" style={{width: "358px", height: "48px", border:"1px solid var(--inner-blue)", marginTop:"20px", borderRadius:"8px"}} />
+            <input type="password" placeholder="Password" className="passwordInput" style={{width: "358px", height: "48px", border:"1px solid var(--inner-blue)", marginTop:"20px", borderRadius:"8px"}} />
+            <input type="password" placeholder="Confirm Password" className="confirmPasswordInput" style={{width: "358px", height: "48px", border:"1px solid var(--inner-blue)", marginTop:"20px", borderRadius:"8px"}} />    
+      </div>
+      <div className="create-account-button" style={{display: "flex", justifyContent: "center", marginTop:"60px"}}>
+        <Button
+        style={{width: "358px", height: "48px", borderRadius:"8px", backgroundColor:"var(--inner-blue)", color:"white", fontSize:"16px", fontWeight:"400", lineHeight:"24px", letterSpacing:"0.15px", fontFamily:"'Inter', sans-serif"}}
+        label="Create Account"
+        onClick={() => router.push("/dashboard")}
+        >
+    
+        </Button>
+      </div>
+
+</div>
     );
 }
