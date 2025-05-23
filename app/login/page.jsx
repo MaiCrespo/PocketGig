3995d9fc5dbff7809bb969064c1e3edf1419a8ab
jordanenyirenda <jnyirenda1@my.bcit.ca>
2025-05-22 20:24:45 -0700

    gogo added, login styled

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -6,26 +6,40 @@ import Button from "@/components/Button/button";
 import TextField from "@/components/TextFeild/TextFeild";
 import styles from "@/app/globals.css"
 import "@/app/login/login.css";
+import InputField from "@/components/InputField";
+
 export default function LoginPage(){
     const router = useRouter();
     return(
     
         <div className="LoginBackground">
-             <div className="LoginImage"></div>
+            <div className="gradient-overlay"></div>
+             <div className="LoginImage">
+                <div className="logo-container">
+                    <Image 
+                        src="/images/logo2.png"
+                        alt="PocketGig Logo"
+                        width={90}
+                        height={36}
+                        priority
+                        quality={100}
+                    />
+                </div>
+            </div>
     
             <h2>Sign-In to PocketGig!</h2>
-            <div className="inputContainer">
-
         
-                <TextField className="inputField">
-                    <label>Email</label>
+            <div className="inputContainer">
+                <InputField id="email" label="Email" sx={{width: "100%", maxWidth: "358px"}}>
                     <input type="email" placeholder="Enter Email..." />
-                </TextField>
+                </InputField> 
 
-                <TextField className="inputField">
-                    <label>Password</label>
-                    <input type="Password" placeholder="Enter Password..." />
-                </TextField>
+                <InputField id="password" label="Password" sx={{width: "100%", maxWidth: "358px", marginTop: "5px"}}>
+                    <input type="password" placeholder="Enter Password..." />
+                </InputField> 
+            </div>
+           
+
                 <div className="buttonContainer">
                     <Button
                     label="Sign-In"
@@ -39,9 +53,11 @@ export default function LoginPage(){
                     textColor="var(--teal-blue)"
                     onClick={() => router.push("/home")}
                     />
+
+
                  
                 </div>
             </div>
-        </div>
+    
     );
 }
